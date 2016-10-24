4c591b72c5a811d0997c2e3e25eb78c1169aa5e3
Dave Amphlett <dave@davelopware.com>
2016-10-25 00:32:36 +0100

    Some logic to try to prevent harvesting source target creeps from blocking full ones from leaving

--- role.harvester.js
+++ role.harvester.js
@@ -6,8 +6,33 @@ var roleHarvester = {
             var sources = creep.room.find(FIND_SOURCES);
             creep.say('harvesting');
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.say('->source');
-                creep.moveTo(sources[0]);
+                // check if we're in a holding pattern
+                if (creep.memory.source_holding_pattern && creep.memory.source_holding_pattern > 0) {
+                    var holdingFlag = Game.flags['SourceHoldingPattern'];
+                    creep.moveTo(holdingFlag);
+                    creep.memory.source_holding_pattern = creep.memory.source_holding_pattern - 1;
+                    creep.memory.source_waiting_for = 0;
+                    creep.say('->hold_'+creep.memory.source_holding_pattern);
+                } else {
+                    // check if we're blocking the source
+                    creep.memory.source_holding_pattern = undefined;
+                    if (creep.pos.getRangeTo(sources[0]) < 4) {
+                        if (creep.memory.source_waiting_for) {
+                            creep.memory.source_waiting_for = creep.memory.source_waiting_for + 1;
+                        } else {
+                            creep.memory.source_waiting_for = 1;
+                        }
+                        creep.say('->src|'+creep.memory.source_waiting_for);
+                    }
+                    if (creep.memory.source_waiting_for > 10) {
+                        creep.memory.source_waiting_for = 0;
+                        creep.memory.source_holding_pattern = 20;
+                    } else {
+                        creep.memory.source_waiting_for = 0;
+                        creep.say('->source');
+                        creep.moveTo(sources[0]);
+                    }
+                }
             }
         }
         else {
